6c3cd65f1f8d8bd87ecd57cd2415bf8b6468ff47
Julian Pira <jpira@unal.edu.co>
2024-05-29 04:02:54 -0500

    Finalized cards desing, header component, structure of: home section, about me section, projects section. And responsive desing

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#1e1e24',
+        secundary: '#788AA3',
+      },
+    },
+
+  },
+  plugins: [],
+};
